03ce9ddba7f50139cf2d6acd17c1d9d0078510b1
Michael Mundy <michael.mundy@intrafinity.loc>
2017-02-24 17:16:08 -0500

    Add Babel transpilation of ES6 to ES5 in start scripts.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,18 +1,18 @@
 //Set up an Express Web server.
 
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
-var port = 3000;
-var app = express(); //Create an instance of Express.
+const port = 3000;
+const app = express(); //Create an instance of Express.
 
 //Send all requests from our root folder to the index.html file.
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 })
 
-//Listen for requests, display an errors, otherwise open browser and display localhost.
+//Listen for requests, display any errors, otherwise open browser and display localhost.
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
